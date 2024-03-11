ffa8bee8fbc46ed5349c5070681a69a9ba5bfcd3
Mayu <mayukha2607@gmail.com>
2024-03-12 00:14:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+import { LoginComponent } from './components/login/LoginComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <LoginComponent/>
+    </>
   );
 }
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
 
 export default App;
---
+++ src/components/login/LoginComponent.js
@@ -0,0 +1,10 @@
+export const LoginComponent = () => {
+    return (
+        <div id="login">
+            <input placeholder="username" type="text"></input>
+            <input placeholder="password" type="password"></input>
+            <button>login</button>
+            <button>sign up</button>
+        </div>
+    )
+}
